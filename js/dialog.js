e7d13822215a7944bde26f8b6c69cd045b06097e
Andrew Zhukevych <hipnos.az@gmail.com>
2018-06-06 18:40:35 +0000

    Moved draged.js module functionality into dialog.js

--- js/dialog.js
+++ js/dialog.js
@@ -6,6 +6,7 @@
   var setupOpen = document.querySelector('.setup-open');
   var setupClose = setup.querySelector('.setup-close');
   var setupUserName = document.querySelector('.setup-user-name');
+  var dialogHandle = setup.querySelector('.setup-user-pic');
 
   var onPopupEscPress = function (evt) {
     if (document.activeElement !== setupUserName) {
@@ -16,6 +17,7 @@
   var openPopup = function () {
     setup.classList.remove('hidden');
     document.addEventListener('keydown', onPopupEscPress);
+    setup.removeAttribute('style');
   };
 
   var closePopup = function () {
@@ -37,6 +39,42 @@
 
   setupClose.addEventListener('keydown', function (evt) {
     window.util.isEnterEvent(evt, closePopup);
+  });
+
+  dialogHandle.addEventListener('mousedown', function (evt) {
+    evt.preventDefault();
+
+    var startCoords = {
+      x: evt.clientX,
+      y: evt.clientY
+    };
+
+    var onMouseMove = function (moveEvt) {
+      moveEvt.preventDefault();
+
+      var shift = {
+        x: startCoords.x - moveEvt.clientX,
+        y: startCoords.y - moveEvt.clientY
+      };
+
+      startCoords = {
+        x: moveEvt.clientX,
+        y: moveEvt.clientY
+      };
+
+      setup.style.top = (setup.offsetTop - shift.y) + 'px';
+      setup.style.left = (setup.offsetLeft - shift.x) + 'px';
+    };
+
+    var onMouseUp = function (upEvt) {
+      upEvt.preventDefault();
+
+      document.removeEventListener('mousemove', onMouseMove);
+      document.removeEventListener('mouseup', onMouseUp);
+    };
+
+    document.addEventListener('mousemove', onMouseMove);
+    document.addEventListener('mouseup', onMouseUp);
   });
 
   window.dialog = document.querySelector('.setup');
