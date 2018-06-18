454c0ca242a3a2557c5a3d6673b00d60d312a249
Andrew Zhukevych <hipnos.az@gmail.com>
2018-06-18 13:36:46 +0300

    Avatar upload functionality added

--- js/dialog.js
+++ js/dialog.js
@@ -1,13 +1,16 @@
 'use strict';
 
-// Opening and closing of modal window
 (function () {
+
+  // Setting used elements & variables
   var setup = document.querySelector('.setup');
   var setupOpen = document.querySelector('.setup-open');
   var setupClose = setup.querySelector('.setup-close');
-  var setupUserName = document.querySelector('.setup-user-name');
+  var setupUserName = setup.querySelector('.setup-user-name');
   var dialogHandle = setup.querySelector('.setup-user-pic');
+  var FILE_TYPES = ['png', 'jpg', 'jpeg', 'gif'];
 
+  // Opening of dialog window
   var onPopupEscPress = function (evt) {
     if (document.activeElement !== setupUserName) {
       window.util.isEscEvent(evt, closePopup);
@@ -41,6 +44,41 @@
     window.util.isEnterEvent(evt, closePopup);
   });
 
+  // Update user avatar
+  var uploadImage;
+  var preview = dialogHandle;
+  var fileChooser = setup.querySelector('.upload input[type=file]');
+
+  var handleFile = function (file) {
+    var fileEnd = file.name.split(/\./);
+    fileEnd = fileEnd[fileEnd.length - 1];
+    if (FILE_TYPES.includes(fileEnd)) {
+      var reader = new FileReader();
+
+      reader.addEventListener('load', function () {
+        preview.src = reader.result;
+      }, false);
+      reader.readAsDataURL(file);
+    }
+  };
+
+  var onFileDrop = function (evt) {
+    evt.preventDefault();
+    evt.stopPropagation();
+    uploadImage = evt.dataTransfer.files[0];
+    handleFile(uploadImage);
+    // console.log('File(s) dropped', uploadImage);
+  };
+
+  var onFileSelect = function (evt) {
+    uploadImage = evt.target.files[0];
+    handleFile(uploadImage);
+  };
+
+  fileChooser.addEventListener('drop', onFileDrop);
+  fileChooser.addEventListener('change', onFileSelect);
+
+  // Dialog window move
   dialogHandle.addEventListener('mousedown', function (evt) {
     evt.preventDefault();
 
